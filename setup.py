6594b13bc48f8c553b8662acef403b85988aac51
Diego Alves <diegoalvesds@outlook.com>
2025-05-03 03:26:05 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="desafio poo",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Diego-Alvesds"
+    url="https://github.com/Diego-Alvesds",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
